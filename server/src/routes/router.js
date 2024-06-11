893da1eea098dcf2b300ceed5bbffd9633bc61f3
annamelya2021 <anna.basyuk@gmail.com>
2024-06-11 12:10:44 +0200

    login/register

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
 import userRouter from "./userRouter.js";
 import { isAuthenticated,isAdmin } from "../middlewares/authMiddleware.js";
+import authRouter from "./authRouter.js";
 
 
 const router  =  Router();
+router.use("/",authRouter);
 router.use("/users",userRouter);
+
 
 
 
