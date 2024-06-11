ba39fd0292cf869f3d8c055af271042e9570ad65
Mikelondrio <mikelon_santxez@hotmail.com>
2024-06-11 15:50:11 +0200

    controlador carritos

--- server/src/controllers/cart/cartController.js
+++ server/src/controllers/cart/cartController.js
@@ -1,50 +1,102 @@
 import cartModel from "../../models/cartModel";
 import productModel from "../../models/productModel";
 
-const getProducts = async (res,req) =>{
-    const products = await productModel.find();
+//crear carrito nuevo para un usuario coprobndo que no tenga ingun carrito abierto
+async function createCart(userId){
+    try {
+        const existingCart = await cartModel.findOne({user:userId, isOpened:true})
 
-    if(products){
-        res.json({products})
-    } else {
-        res.json( {message : "there are no products"} );
+        if(existingCart){
+            return existingCart
+        }
+        const cart = {
+            user:userId,
+            isOpened:true
+        }
+        const newCart = await cartModel.create(cart)
+        return newCart
+        
+    } catch (error) {
+        console.error(error);
+        return {error:"There was an error creating the cart"};
     }
-
-};
-
-const getProductsCart = async (res,req) =>{
-    const productsCart = await cartModel.find();
-
-    if(productsCart){
-        res.json({productsCart})
-    } else {
-        res.json( {message : "there are no products in the cart"} );
+}
+//cambiar el booleano isOpened/ enviar correo al usuaurio
+async function closeCart(cartId){
+    try {
+        const cart = await cartModel.findOneAndUpdate({_id:cartId},{isOpened:false})
+        return cart
+    } catch (error) {
+        console.error(error);
+        return {error:"There was an error closing the cart"};
     }
-
-};
-//Tener en cuenta lo que hay dentro del modelo
-const addProductCart = async (res,req) =>{
-    const  {product_image, product_name, product_description, product_model, product_type, product_price} = req.body;
-
-    const isInProducts = productModel.findOne({product_name});
-
-    const isNotEmpty = product_image!== "" && product_name!== "" && product_description!== "" && product_model!== "" && product_type!== "" && product_price!== "";
-
-    const isInCart = cartModel.findOne({product_name});
-
-    if(!isInProducts){
-        res.status(400).json({
-            message : "This product is not in our database"
-        });
+}
+//
+async function getCartOpened(userId){
+    try {
+    const openCarts = await cartModel.find({cartUser: userId, isOpened:true})
+    if (openCarts.length===0){
+        const cart = await createCart(userId);
+        return cart
     }
-    else if(isNotEmpty && !isInCart){
-        const newProductInCart = new Cart({product_image, product_name, product_description, product_model, product_type, product_price})
-
+    return openCarts[0]
+    
+} catch (error) {
+    console.error(error);
+        return {error:"There was an error getting the opened cart"};
+}
+}
+//devolver array de carritos
+//Historial de carritos
+async function getCarts(userId){
+    try {
+        const cartHistory = await cartModel.find({cartUser: userId, isOpened:false})
+        return cartHistory;
+    } catch (error) {
+        console.error(error);
+        return {error:"There was an error getting the cart history"};
     }
 }
 
+async function addProductToCart(productId,cartId){
+    try {
+        const cart = await cartModel.findById(cartId)
+        const product = await productModel.findById(productId)
+        cart.products.push(product)
+        await cart.save()
+        return cart
+        
+    } catch (error) {
+        console.error(error);
+        return {error:"There was an error adding product to cart"};
+    }
+}
+async function removeProductFromCart(productId,cartId){
+    try {
+        const cart = await cartModel.findById(cartId)
+        const product = await productModel.findById(productId)
+        cart.products.pull(product)
+        await cart.save()
+        return cart
+        
+    } catch (error) {
+        console.error(error);
+        return {error:"There was an error removing product from cart"};
+    }
+}
+//utilizar un find para borrar solo el primero que encuentre
+
+
+
+
+
+
 export const functions ={
-    getProducts,
-    getProductsCart,
+    createCart,
+    closeCart,
+    getCartOpened,
+    getCarts,
+    addProductToCart,
+    removeProductFromCart
 
 }
--- server/src/models/cartModel.js
+++ server/src/models/cartModel.js
@@ -1,24 +1,21 @@
 import mongoose from "mongoose";
 
 const cartSchema  = new mongoose.Schema({
-    cartProduct: {
+    cartProducts: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "products",
-        required: true
-    },
-    cartProduct_amount:{
-        type: Number,
-        required: true
-    },
+    }],
     cartUser: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "users",
         required: true
     },
-    cart_totalPrice:{
-        type: Number,
-        required: true
+    isOpened: {
+        type: Boolean,
+        default:true
+
     }
+   // calcular total price fuera
 })
 
 const cartModel = mongoose.model("carts",cartSchema);
