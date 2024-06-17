f5505343f56c7929d410ee75a592a2e1a698c61f
annamelya2021 <anna.basyuk@gmail.com>
2024-06-17 20:33:10 +0200

    .

--- client/src/pages/AdminPanel/AdminPanel.jsx
+++ client/src/pages/AdminPanel/AdminPanel.jsx
@@ -1,31 +1,36 @@
 import { useState, useContext } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Modal from "../../components/modal/Modal";
 import CreateProduct from "../../components/product/CreateProduct";
-import UserContext from "../../context/userContext"; 
+import UserContext from "../../context/userContext";
 
 const AdminPanel = () => {
-    const { user } = useContext(UserContext); 
     const [products, setProducts] = useState(useLoaderData());
     const [creatingProduct, setCreatingProduct] = useState(false);
+    const { user } = useContext(UserContext);
+
+    const handleCreateProduct = (newProduct) => {
+        setCreatingProduct(false);
+    };
 
     return (
         <>
             <h1>Welcome Administrator</h1>
-        
-            {creatingProduct ?
+            {creatingProduct ? (
                 <Modal onClose={() => setCreatingProduct(false)}>
-                    <CreateProduct onCreate={() => setCreatingProduct(false)} />
+                    <CreateProduct onCreate={handleCreateProduct} />
                 </Modal>
-                :
+            ) : (
                 <button onClick={() => setCreatingProduct(true)}>New Product</button>
-            }
+            )}
 
             <div>
-                <p>We have {user ? user.length : 0} users registered</p>
+                <p>We have {user.length} users registered</p>
             </div>
+
+            
         </>
     );
-}
+};
 
 export default AdminPanel;
