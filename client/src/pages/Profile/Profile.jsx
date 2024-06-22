f188dd5b970ae2b8b826c1cee9dcf930d897a124
annamelya2021 <anna.basyuk@gmail.com>
2024-06-22 17:04:21 +0200

    .

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -3,14 +3,15 @@ import Popup from 'reactjs-popup';
 import { getCarts, update, getProduct } from '../../utils/fetch';
 import './Profile.css';
 import UserContext from '../../context/userContext';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
 
 const Profile = () => {
     const { user, setUser } = useContext(UserContext);
     const [carts, setCarts] = useState([]);
     const [favoriteProducts, setFavoriteProducts] = useState([]);
-    const [loading, setLoading] = useState(true); // Доданий стейт для відображення загрузки
+    const [loading, setLoading] = useState(true); 
 
     // Fetch bought carts history
     useEffect(() => {
@@ -44,10 +45,10 @@ const Profile = () => {
                     })
                 );
                 setFavoriteProducts(products);
-                setLoading(false); // Відключаємо загрузку після завершення запиту
+                setLoading(false); 
             } catch (error) {
                 console.error('Failed to load favorite products:', error);
-                setLoading(false); // Обробка помилок загрузки
+                setLoading(false); 
             }
         };
 
@@ -65,13 +66,13 @@ const Profile = () => {
             const result = await update(userData);
             if (result && result.data) {
                 setUser(result.data);
-                toast.success('User updated successfully!');
+                alert('User updated successfully!');
             } else {
                 alert('Error updating user.');
             }
         } catch (error) {
             console.error('Error updating user:', error);
-            toast.error('Error updating user.');
+            alert('Error updating user.');
         }
     };
 
@@ -93,6 +94,7 @@ const Profile = () => {
 
     return (
         <>
+        
         <div className="profile-container">
             {/* User Information */}
             <article className="section-container user-card" key={user._id}>
